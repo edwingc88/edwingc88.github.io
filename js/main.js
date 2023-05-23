fb13ff5af69670db2c8a14b82aa9f244fbb40e6e
unknown <edwingc88@gmail.com>
2023-05-23 11:43:23 -0400

    modedark and other

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 const body  = document.querySelector("body");
 const toggle = document.querySelector("#toggle-mode");
-/*const main  = document.querySelector("#inicio");
-const menu = document.querySelectorAll(".menu_link");
+const $a  = document.querySelectorAll(".main a");
+const $p  = document.querySelectorAll(".main p");
+const $h1Title= document.querySelector("#inicio > header > div.header_info > h1")
+/*const menu = document.querySelectorAll(".menu_link");*/
 
-console.log(body)*/
+console.log(body);
 toggle.onclick = function(){
     toggle.classList.toggle("switch");
     body.classList.toggle("switch");
+    $h1Title.classList.toggle("switch_title");
+    for(const element of $a) {
+        console.log(element); 
+        element.classList.toggle("switch");
+    }
+    for(const element of $p) {
+        console.log(element); 
+        element.classList.toggle("switch");
+    }
+    
 }
-/*
-for(const element of menu) {
-    element.addEventListener('click', ()=>{
-        for(const elementDeleteClass of menu) {
-            elementDeleteClass.classList.remove('activo')
-        }    
-        element.classList.add('activo');
-    })
-}*/
-
-console.log("hello"); 
